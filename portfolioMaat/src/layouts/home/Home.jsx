d8467f62747b6f848ddd6f194082534969d3fb59
Mario Aguilar <marioaguilar740@gmail.com>
2023-09-14 14:39:56 +0200

    Modificamos el tiempo del setTimeout para que este mas tiempo la presentación

--- portfolioMaat/src/layouts/home/Home.jsx
+++ portfolioMaat/src/layouts/home/Home.jsx
@@ -5,23 +5,24 @@ export const Home = () => {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-    // Simula un tiempo de carga
-    setTimeout(() => {
-        setLoading(false);
-    }, 3000); // Cambia este valor al tiempo que desees
+        // Simula un tiempo de carga
+        setTimeout(() => {
+            setLoading(false);
+        }, 5000); // Cambia este valor al tiempo que desees
     }, []);
 
 return (
-    <div>
-      {loading ? (
-        <InicioAnim />
-      ) : (
-        // Renderiza el contenido principal de tu aplicación aquí
+    <>
         <div>
-          <h1>Bienvenido a mi sitio web</h1>
-          {/* ...otros componentes y contenido */}
+            {loading ? (
+            <InicioAnim />
+            ) : (
+                // Renderiza el contenido principal de tu aplicación aquí
+                <div>
+                    <h1>Bienvenido a mi sitio web</h1>
+                </div>
+            )}
         </div>
-      )}
-    </div>
+    </>
 )
 }
