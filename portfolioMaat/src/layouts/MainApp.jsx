f551c9b2df2011a743811361bb687ea21270b4f4
Mario Aguilar <marioaguilar740@gmail.com>
2023-09-15 20:25:45 +0200

    Se agrego el componente Navbar a las paginas

--- portfolioMaat/src/layouts/MainApp.jsx
+++ portfolioMaat/src/layouts/MainApp.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Route, Routes, Navigate } from 'react-router-dom'
+import { NavBar } from '../components/NavBar'
 import { Home } from './home/home'
 
 export const MainApp = () => {
   return (
     <>
+    <NavBar/>
     <Routes>
         <Route path='/' element={<Home/>}/>
     </Routes>
